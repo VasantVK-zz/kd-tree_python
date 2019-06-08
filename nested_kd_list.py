d1bbe0b3e2cafe674ae9294f3fce5a09e7b8dfd4
VasantVK <32724173+VasantVK@users.noreply.github.com>
2019-06-07 21:00:50 -0700

    0.3
    Almost functional

--- nested_kd_list.py
+++ nested_kd_list.py
@@ -9,19 +9,19 @@ def insertion(point, tree_node, d, trav): # Parameters: (Point, Tree, Dimension,
 	dim = trav % d
 	if (point[dim] < tree_node[0][dim]) and (tree_node[1] == None):
 		new_node = [point, None, None, trav]
-	elif (point[dim] >= tree_node[0][dim]) and (tree_node[1] != None): 
-		return insertion(point, tree_node[1], d, trav + 1)
 		tree_node[1] = new_node
+	elif (point[dim] >= tree_node[0][dim]) and (tree_node[1] != None): 
+		insertion(point, tree_node[1], d, trav + 1)
 	if (point[dim] >= tree_node[0][dim]) and (tree_node[2] == None): 
 		new_node = [point, None, None, trav]
 		tree_node[2] = new_node
 	elif (point[dim] >= tree_node[0][dim]) and (tree_node[2] != None):  
-		return insertion(point, tree_node[2], d, trav + 1) 
+		insertion(point, tree_node[2], d, trav + 1) 
 
 	return tree_node
 
 def nested_kd_list(x, d):
-	tree_node = [None, None, None, 0] # Structure: [Point, Left Node, Right Node, Current Depth]
+	tree_node = [None, None, None, -1] # Structure: [Point, Left Node, Right Node, Current Depth]
 	
 	i = 0
 	while i < len(x):
