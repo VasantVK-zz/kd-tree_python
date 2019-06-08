a0c6196a5aad87bb52842308701b6a414c85f513
VasantVK <32724173+VasantVK@users.noreply.github.com>
2019-06-08 12:57:28 -0700

    1.2.1
    Attemping to create a plot with the list

--- nested_kd_list.py
+++ nested_kd_list.py
@@ -2,24 +2,27 @@
 # Begun: 06/06/19
 # Last Update: 06/07/19
 
+import matplotlib.pyplot as plt
 from random import *
 from math import *
 
-def insertion(point, tree_node, trav): # Parameters: (Point, Tree, Current Depth)
-	dim = trav % d
+
+def insertion(point, tree_node, cur_dep): # Parameters: (Point, Tree, Current Depth)
+	dim = cur_dep % d
 
 	if (point[dim] < tree_node[0][dim]):
 		if (tree_node[1] == None):
 			new_node = [point, None, None]
 			tree_node[1] = new_node
 		else:
-			insertion(point, tree_node[1], trav + 1)
+			insertion(point, tree_node[1], cur_dep + 1)
+			
 	if (point[dim] >= tree_node[0][dim]):
 		if  (tree_node[2] == None):
 			new_node = [point, None, None]
 			tree_node[2] = new_node
 		else: 
-			insertion(point, tree_node[2], trav + 1) 
+			insertion(point, tree_node[2], cur_dep + 1) 
 
 	return tree_node
 
@@ -31,25 +34,46 @@ def nested_kd_list(x):
 		if i == 0:
 			tree_node[0] = x[i]
 		else:
-			trav = 0
-			#print(x[i])
-			tree_node = insertion(x[i], tree_node, trav)
-		#print(tree_node)
+			cur_dep = 0
+			tree_node = insertion(x[i], tree_node, cur_dep)
+		print(tree_node)
 		
 		i += 1
 
 	return tree_node
 
+# Attempting to create a recursive-like structure to create a plot
+def kd_plot(node_list):
+	dep = 0
+	dep_list = []
+	i_list = []
+
+	for i in range(0, len(node_list)-1):
+		if isinstance(node_list[i], int):
+			# plot
+		else:
+			dep_list.append(dep)
+			i_list.append(i)
+		if len(i_list) == 0 and i_list[len(i_list)-1] != 2:
+
+
+
 def main():
-	points1D = [(1,), (3,), (5,), (5,), (2,)]
+	# Non-Random Tests
+	# points1D = [(1,), (3,), (5,), (5,), (2,)]
 	points2D = [(1,10), (3,5), (5,4), (5,3), (2,8)]
-	points3D = [(1,10,7), (3,5,3), (5,4,0), (5,3,6), (2,8,9)]
-	pointList = [points1D, points2D, points3D]
-	#dimensions = 2
-	for sample_points in pointList:
-		dimensions = len(sample_points[0])
-		global d
-		d = dimensions
-		nested_kd_list(sample_points)
+	# points3D = [(1,10,7), (3,5,3), (5,4,0), (5,3,6), (2,8,9)]
+	# pointList = [points1D, points2D, points3D]
+	dimensions = 2
+	# for sample_points in pointList:
+	# 	dimensions = len(sample_points[0])
+	global d
+	d = dimensions
+	node_list = nested_kd_list(points2D)
+
+	if d == 2:
+		kd_plot(node_list)
+
+
 
 main()
