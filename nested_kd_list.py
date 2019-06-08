85896696d2d3fed6b130571b8b6bcc862e2069a0
VasantVK <32724173+VasantVK@users.noreply.github.com>
2019-06-07 21:28:09 -0700

    1.1
    1D, 2D, 3D Functional

--- nested_kd_list.py
+++ nested_kd_list.py
@@ -1,6 +1,6 @@
 # Created by Vasant Kurvari
 # Begun: 06/06/19
-# Last Update: 06/06/19
+# Last Update: 06/07/19
 
 from random import *
 from math import *
@@ -29,17 +29,22 @@ def nested_kd_list(x, d):
 			tree_node[0] = x[i]
 		else:
 			trav = 0
-			print(x[i])
+			#print(x[i])
 			tree_node = insertion(x[i], tree_node, d, trav)
-		print(tree_node)
+		#print(tree_node)
 		
 		i += 1
 
 	return tree_node
 
 def main():
-	points1 = [(1,10), (3,5), (5,4), (5,3), (2,8)]
-	dimensions = 2
-	nested_kd_list(points1, dimensions)
+	points1D = [(1,), (3,), (5,), (5,), (2,)]
+	points2D = [(1,10), (3,5), (5,4), (5,3), (2,8)]
+	points3D = [(1,10,7), (3,5,3), (5,4,0), (5,3,6), (2,8,9)]
+	pointList = [points1D, points2D, points3D]
+	#dimensions = 2
+	for sample_points in pointList:
+		dimensions = len(sample_points[0])
+		nested_kd_list(sample_points, dimensions)
 
 main()
